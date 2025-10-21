c0616dff6108b3bab6c8a0fd1cc174fccf9ae135
Dahlia Radif <dahlia@felixpago.com>
2025-10-21 14:47:32 -0700

    setting mcp server in env

--- transaction_status_agent/config.py
+++ transaction_status_agent/config.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
+
 MODEL_NAME = "gemini-2.0-flash"
 APP_NAME = "transaction_status_agent"
 DEFAULT_USER_ID = "user_12345"
-MCP_SERVER_URL = "http://127.0.0.1:3333/sse"
+MCP_SERVER_URL = os.getenv("MCP_SERVER_URL")
+
